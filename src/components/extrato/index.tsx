cfdb99f185ce469cdd1cc4b9af76108d9b491a3b
Rodrigo Dias <rodrigovicente.dev@gmail.com>
2022-11-19 23:04:23 -0300

    criado o extrato, pagina principal completa

--- src/components/extrato/index.tsx
+++ src/components/extrato/index.tsx
@@ -1,11 +1,116 @@
+import { useEffect, useState } from "react";
+import { api } from "../../config/api";
+
 interface Props {
-    reload: boolean;
-    setReload: React.Dispatch<React.SetStateAction<boolean>>;
+  reload: boolean;
+  setReload: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+
+interface User {
+    id: number;
+    username: String;
+    accounts: {
+      id: number;
+      balance: number;
+    };
   }
 
 function Extrato({ reload, setReload }: Props) {
-    return (<>
-    </>  );
+  const [extrato, setExtrato] = useState([]);
+  const [user, setUser] = useState<User>({} as User);
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(false);
+    async function fetchuextrato() {
+      try {
+        const response = await api.get("/extrato");
+        setExtrato(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setloading(true);
+    }
+    fetchuextrato();
+  }, [reload]);
+
+
+  
+  useEffect(() => {
+    setloading(false);
+ async function fetchuser() {
+   
+   try {
+     const response = await api.get("/usuario");
+     setUser(response.data[0]);
+   } catch (error) {
+     console.log(error);
+   }setloading(true);
+ }
+ fetchuser();
+ 
+}, []);
+
+
+  console.log(extrato);
+
+  return (
+    <>
+      <div>
+        <table>
+          <tr>
+            <td>Conta</td>
+            <td>valor</td>
+            <td>Data</td>
+          </tr>
+
+          {loading &&
+            extrato.map((element) => {
+              const date = new Date(element.createdAt);
+              let dd = date.getDate();
+              let mm = date.getMonth();
+              let yyy = date.getFullYear();
+              let data = `${dd + 1}/${mm + 1}/${yyy}`;
+              let conta = 0
+              let valor =""
+              
+              if(element.creditedAccountId.id === user.id){
+                conta = element.debitedAccountId.id
+                valor = `+ ${element.value}`
+
+              }
+
+              if(element.debitedAccountId.id === user.id){
+                conta = element.creditedAccountId.id
+                valor = `- ${element.value}`
+
+              }
+                
+              console.log(element.debitedAccountId.id)
+              console.log(user.id)
+              return (
+                <>
+                  <tr>
+                    <td>{conta}</td>
+                    <td style={{color: valor[0] === "+"?"blue":"red"}}>{valor}</td>
+                    <td>{data}</td>
+                  </tr>{" "}
+                </>
+              );
+            })}
+
+          <td>Ted</td>
+          <td>22</td>
+
+          <tr>
+            <td>Ralf</td>
+            <td>26</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default Extrato;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,52 +1,57 @@
 import { useNavigate } from "react-router-dom";
-import {useState} from "react"
+import { useState } from "react";
 import { api } from "../../config/api";
 import { Link } from "react-router-dom";
 
-interface Form{
-    username: string,
-    password: string,
-   
-
- }
+interface Form {
+  username: string;
+  password: string;
+}
 
 function Login() {
-   
-    
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [form, setForm] = useState<Form>({
     username: "",
     password: "",
   });
 
-  function handleChange(e:React.ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setForm({ ...form, [e.target.name]: e.target.value });
   }
 
-  async function handleSubmit(e:React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
       const response = await api.post("/login", form);
       localStorage.setItem("loggedInUser", JSON.stringify(response.data));
-      
     } catch (error: any) {
       console.log(error);
       window.alert(error.response.data.message);
     }
+    navigate("/principal");
   }
-    return ( <>
-
-
-<form onSubmit={handleSubmit}>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
         <label>UserName</label>
-        <input type="text" name="username" pattern="\w{3,}" onChange={handleChange} />
+        <input
+          type="text"
+          name="username"
+          pattern="\w{3,}"
+          onChange={handleChange}
+        />
         <label>Password</label>
-        <input type= "password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#_!])[0-9a-zA-Z$*&@#_!]{8,}$" onChange={handleChange} name={"password"} />
+        <input
+          type="password"
+          pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#_!])[0-9a-zA-Z$*&@#_!]{8,}$"
+          onChange={handleChange}
+          name={"password"}
+        />
         <button type="submit">Enviar</button>
       </form>
-<Link to="/sigin">Criar Usuario</Link>
-
-    </> );
+      <Link to="/sigin">Criar Usuario</Link>
+    </>
+  );
 }
 
 export default Login;
